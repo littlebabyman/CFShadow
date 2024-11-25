764caa4b23e6c1e491e224595dc52ed92a400f4d
afxnatic <174385871+afxnatic@users.noreply.github.com>
2024-11-24 16:56:08 -0800

    clegs offset + better drawing
    -- applies render origin offset from clegs if present via grabbing ent.RenderPos
    -- player angle excludes pitch and roll now
    -- aim_yaw pose parameter is no longer applied, it looks weird in fp

--- lua/entities/cfshadow_shadow_player.lua
+++ lua/entities/cfshadow_shadow_player.lua
@@ -25,50 +25,78 @@ function ENT:Initialize()
 
     -- MsgC(color_white, "[", Color(200, 0, 0), "CFShadow", color_white, "] - Firstperson player shadow's player validity check passed!", "\n")
 
-    self:SetAutomaticFrameAdvance(true)
-    self:SetMoveType(MOVETYPE_NONE)
     self:DrawShadow(true)
+    self:SetAutomaticFrameAdvance(true)
     self:SetRenderMode(RENDERMODE_NORMAL)
+    self:SetMoveType(MOVETYPE_NONE)
     self:SetMaterial("engine/occlusionproxy")
+    -- self:SetMaterial("editor/wireframe")
 end
 
 local ENTITY = FindMetaTable("Entity")
 local PLAYER = FindMetaTable("Player")
 local eGetModelScale, eSetModelScale = ENTITY.GetModelScale, ENTITY.SetModelScale
+local eGetTable = ENTITY.GetTable
 local eGetPos, eSetPos = ENTITY.GetPos, ENTITY.SetPos
 local eGetAngles, eSetAngles = ENTITY.GetAngles, ENTITY.SetAngles
 local eGetSequence, eSetSequence = ENTITY.GetSequence, ENTITY.SetSequence
 local eGetCycle, eSetCycle = ENTITY.GetCycle, ENTITY.SetCycle
 local eGetNumPoseParameters = ENTITY.GetNumPoseParameters
+local eGetPoseParameterName = ENTITY.GetPoseParameterName
 local eGetPoseParameterRange = ENTITY.GetPoseParameterRange
 local eGetPoseParameter, eSetPoseParameter = ENTITY.GetPoseParameter, ENTITY.SetPoseParameter
 local eInvalidateBoneCache = ENTITY.InvalidateBoneCache
 local eGetNumBodyGroups = ENTITY.GetNumBodyGroups
 local eGetBodygroup, eSetBodygroup = ENTITY.GetBodygroup, ENTITY.SetBodygroup
 local eSetNextClientThink = ENTITY.SetNextClientThink
-local aCurTime = CurTime
+local poseParameterBlacklist = {
+    ["aim_yaw"] = true
+}
 local haveLayeredSequencesBeenFixed = false
 local lastBodygroupApply = 0
 
 function ENT:Think()
     eSetModelScale(self, eGetModelScale(ply))
-    eSetPos(self, eGetPos(ply))
-    eSetAngles(self, eGetAngles(ply))
+
+    local pos = eGetPos(ply)
+    local legs = eGetTable(ply).LegEnt
+
+    if IsValid(legs) then
+        local legsTable = eGetTable(legs)
+
+        if legsTable.DidDraw then
+            pos = legsTable.RenderPos
+        end
+    end
+
+    eSetPos(self, pos)
+
+    local ang = eGetAngles(ply)
+    ang.p = 0
+    ang.r = 0
+
+    eSetAngles(self, ang)
     eSetSequence(self, eGetSequence(ply))
 
     -- ISSUE: https://github.com/Facepunch/garrysmod-requests/issues/1723
     if haveLayeredSequencesBeenFixed then
-        self:CopyLayerSequenceInfo(0, ply)
-        self:CopyLayerSequenceInfo(1, ply)
-        self:CopyLayerSequenceInfo(2, ply)
-        self:CopyLayerSequenceInfo(3, ply)
-        self:CopyLayerSequenceInfo(4, ply)
-        self:CopyLayerSequenceInfo(5, ply)
+        self:CopyLayerSequence(0, ply)
+        self:CopyLayerSequence(1, ply)
+        self:CopyLayerSequence(2, ply)
+        self:CopyLayerSequence(3, ply)
+        self:CopyLayerSequence(4, ply)
+        self:CopyLayerSequence(5, ply)
     end
 
     eSetCycle(self, eGetCycle(ply))
 
     for i = 0, eGetNumPoseParameters(ply) - 1 do
+        local name = eGetPoseParameterName(ply, i)
+
+        if poseParameterBlacklist[name] then
+            continue
+        end
+
         local min, max = eGetPoseParameterRange(ply, i)
 
         eSetPoseParameter(self, i, math.Remap(eGetPoseParameter(ply, i), 0, 1, min, max))
@@ -76,7 +104,7 @@ function ENT:Think()
 
     eInvalidateBoneCache(self)
 
-    local curTime = aCurTime()
+    local curTime = CurTime()
 
     if lastBodygroupApply + 1.0 < curTime then
         for i = 1, eGetNumBodyGroups(ply) do
@@ -93,7 +121,7 @@ function ENT:Think()
     return true
 end
 
-function ENT:CopyLayerSequenceInfo(layer, fromEnt)
+function ENT:CopyLayerSequence(layer, fromEnt)
     self:SetLayerSequence(layer, fromEnt:GetLayerSequence(layer))
     self:SetLayerDuration(layer, fromEnt:GetLayerDuration(layer))
     self:SetLayerPlaybackRate(layer, fromEnt:GetLayerPlaybackRate(layer))
@@ -103,6 +131,7 @@ end
 
 local eDestroyShadow = ENTITY.DestroyShadow
 local pAlive = PLAYER.Alive
+local pInVehicle = PLAYER.InVehicle
 local pShouldDrawLocalPlayer = PLAYER.ShouldDrawLocalPlayer
 local pFlashlightIsOn = PLAYER.FlashlightIsOn
 local rGetName = FindMetaTable("ITexture").GetName
@@ -122,7 +151,7 @@ function ENT:Draw()
     end
 
     -- COMMENT
-    if pShouldDrawLocalPlayer(ply) then
+    if pInVehicle(ply) or pShouldDrawLocalPlayer(ply) then
         return
     end
 
@@ -156,7 +185,7 @@ function ENT:Draw()
 
     eDrawModel(self)
 
-    -- FIXME: Why do we have to do this manually?
+    -- -- FIXME: Why do we have to do this manually?
     eCreateShadow(self)
 end
 
